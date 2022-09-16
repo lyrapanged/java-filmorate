653b2865ab2c93732f08bca0904e1ce42e955fa1
lyrapanged <bogart0@yandex.ru>
2022-09-16 23:12:38 +0300

    Sprint 9.0.0

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -0,0 +1,68 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@RestController
+@RequestMapping("/films")
+@Slf4j
+public class FilmController {
+    private final Map<Integer,Film> films = new HashMap<>();
+    private Integer idFilm = 0;
+    private final static int DESCRIPTION_LENGTH = 200;
+    private final static LocalDate LOWER_DATE = LocalDate.of(1895, 12, 28);
+
+    @PostMapping()
+    public Film addFilm(@RequestBody Film film) {
+        validationFilm(film);
+        film.setId(++idFilm);
+        films.put(film.getId(),film);
+        log.info("Film added.");
+        return film;
+    }
+
+    @PutMapping()
+    public Film updateFilm(@RequestBody Film film) {
+        validationFilm(film);
+        if (films.get(film.getId()) == null) {
+            log.error("Bad id");
+            throw new ValidationException("ID doesn't exist");
+        }
+        films.put(film.getId(),film);
+        log.info("Film updated.");
+        return film;
+    }
+
+    @GetMapping()
+    public List<Film> getFilms() {
+        log.info("Request getFilms");
+        return new ArrayList<>(films.values());
+    }
+
+    private void validationFilm(Film film) {
+        if (film.getName().isBlank()) {
+            log.error("Bad name.");
+            throw new ValidationException("Name cannot be empty.");
+        }
+        if (film.getDescription().length() > DESCRIPTION_LENGTH) {
+            log.error("Bad description.");
+            throw new ValidationException("Max description length is 200 symbols.");
+        }
+        if (film.getReleaseDate().isBefore(LOWER_DATE)) {
+            log.error("Bad release date");
+            throw new ValidationException("Release date - no earlier than December 28, 1895.");
+        }
+        if (film.getDuration() <= 0){
+            log.error("Bad duration.");
+            throw new ValidationException("Film duration must be positive.");
+        }
+        log.info("Validation passed successfully.");
+    }
+}
+
+
---
+++ src/test/java/ru/yandex/practicum/filmorate/controllersTest/UserControllerTest.java
@@ -0,0 +1,61 @@
+package ru.yandex.practicum.filmorate.controllersTest;
+
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controllers.UserController;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class UserControllerTest {
+    private final User user = User.builder()
+            .id(780)
+            .email("@dada")
+            .login("dadaya")
+            .name("da")
+            .birthday(LocalDate.of(2020, 2, 22))
+            .build();
+    ;
+    private UserController userController = new UserController();
+
+    @Test
+    void successfulValidation() {
+        assertDoesNotThrow(() -> userController.addUser(user), "Ooops");
+        assertDoesNotThrow(() -> userController.updateUser(user), "Ooops");
+    }
+
+    @Test
+    void validationTestEmailContainBadSymbols() {
+        user.setEmail("dada");
+        assertThrows(ValidationException.class, () -> userController.addUser(user), "Email contains \"@\"");
+    }
+
+    @Test
+    void validationTestEmailIsBlank() {
+        user.setEmail("        ");
+        assertThrows(ValidationException.class, () -> userController.addUser(user), "Email is blank");
+    }
+
+
+    @Test
+    void validationTestLoginIsBlank() {
+        user.setLogin("    ");
+        assertThrows(ValidationException.class, () -> userController.addUser(user), "Login is blank");
+    }
+
+    @Test
+    void validationTestLoginContainBadSymbols() {
+        user.setLogin("dada dada");
+        assertThrows(ValidationException.class, () -> userController.addUser(user), "Login contain space");
+    }
+
+    @Test
+    void validationTestBadBirthday() {
+        user.setBirthday(LocalDate.now());
+        assertThrows(ValidationException.class, () -> userController.addUser(user), "Login contain space");
+    }
+
+}
