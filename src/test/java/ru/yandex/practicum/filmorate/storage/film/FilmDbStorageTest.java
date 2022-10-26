fe6dcd4cf1e38609f15ab6d123d5bb301d1c9b17
lyrapanged <bogart0@yandex.ru>
2022-10-26 07:08:46 +0300

    12.002

--- src/test/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorageTest.java
@@ -1,16 +1,8 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmDbStorageTest {
 
-    private final FilmDbStorage filmDbStorage;
+
 
 /*    @BeforeEach
     void setUp() {
