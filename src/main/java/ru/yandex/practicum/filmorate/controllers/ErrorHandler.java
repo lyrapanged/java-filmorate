d93a87ac10cdb50803d4ad49eb7edc5f11b8265c
lyrapanged <bogart0@yandex.ru>
2023-01-25 17:23:48 +0300

    refactor: Exception Handler

--- src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
@@ -5,42 +5,53 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 
 import javax.validation.ConstraintViolationException;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
+
     @ExceptionHandler
-    public ResponseEntity<String> handleValidationException(final ValidationException e) {
-        log.info("Invalid validation");
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> notFoundException(final NotFoundException e) {
+        String result = e.getMessage();
+        log.info("Entity doesn't exist {}", e.getMessage());
+        return Map.of("Error", result);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public Map<String, String> ex(final MethodArgumentNotValidException e) {
+        log.info("MethodArgumentNotValidException{}", e.getMessage());
+        String result = e.getMessage();
+        return Map.of("Error", result);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public Map<String, String> exe(final ValidationException e) {
+        log.info("ValidationException {}", e.getMessage());
+        String result = e.getMessage();
+        return Map.of("Error", result);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public Map<String, String> rer(final ConstraintViolationException e) {
+        log.info("ConstraintViolationException {}", e.getMessage());
+        String result = e.getMessage();
+        return Map.of("Error", result);
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> exc(final ConstraintViolationException e) {
-        log.info("Incorrect variable count");
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<String> notFoundException(final NotFoundException e) {
-        log.info("Entity doesn't exist");
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<String> ex(final MethodArgumentNotValidException e) {
-        log.info("Unhandled exception");
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<String> unhandledException(final Throwable e) {
-        log.error("Unhandled exception");
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<StackTraceElement[]> unhandledException(final Throwable e) {
+        log.error("Unhandled exception{}", e.getMessage());
+        return new ResponseEntity<>(e.getStackTrace(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
